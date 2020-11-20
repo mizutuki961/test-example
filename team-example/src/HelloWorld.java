4faaa33b1ae68d797cda4a5bf25ef9e70a3027b2
Kitagawa Miduki <kd1298549@st.kobedenshi.ac.jp>
2020-11-20 14:35:11 +0900

    初期コミット

---
+++ team-example/src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author 0H03015 北川水月
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
