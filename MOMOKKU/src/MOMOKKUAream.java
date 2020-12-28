070ce28b5834d5524ac414653c5793bedeead038
Kitagawa Miduki <kd1298549@st.kobedenshi.ac.jp>
2020-12-28 14:05:25 +0900

    初期状態

---
+++ MOMOKKU/src/MOMOKKUAream.java
@@ -0,0 +1,52 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+/**
+ * 
+ */
+
+/**
+ * @author 0H03015 ñkêÏêÖåé
+ *
+ */
+public class MOMOKKUAream extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MOMOKKUAream frame = new MOMOKKUAream();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MOMOKKUAream() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
